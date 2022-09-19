2e1e5ec7255378177951b5dc4acd4aa1c80d35f5
Kevin Roldos <kevinr@iri.com>
2022-09-19 09:41:43 -0400

    update

--- RowGen-BankSQL/SavedScripts/new_batch/new_batch.bat
+++ RowGen-BankSQL/SavedScripts/new_batch/new_batch.bat
@@ -1,12 +1,21 @@
 @echo off
 SETLOCAL
-sqlcmd -U IRIDEMO -P sorco78 -S 192.168.3.95,1444 -d support -i DisableFK_Multiple_Tables.sql
+sqlcmd -U BANK -P Sorco789 -S 192.168.3.95,1444 -d support -i DisableFK_Multiple_Tables.sql
 
+sortcl /SPECIFICATION=TRXN_CODE_UNIQUE.rcl
 sortcl /SPECIFICATION=BRANCH_ID_UNIQUE.rcl
-sortcl /SPECIFICATION=TRANSACTION_CODE_UNIQUE.rcl
 sortcl /SPECIFICATION=CUSTOMER_ID_UNIQUE.rcl
 sortcl /SPECIFICATION=ACCOUNT_NUMBER_UNIQUE.rcl
-sortcl /SPECIFICATION=TRANSACTION_ID_UNIQUE.rcl
+sortcl /SPECIFICATION=TRXN_ID_UNIQUE.rcl
+ping 192.0.2.0 -n 1 -w 3000 >NUL
+if not exist "TRXN_CODE.sed" (
+    echo file "TRXN_CODE.sed" not found.
+    echo Job terminated
+    exit
+) else (
+    echo file "TRXN_CODE.sed" found.
+    echo:
+)
 ping 192.0.2.0 -n 1 -w 3000 >NUL
 if not exist "BRANCH_ID.sed" (
     echo file "BRANCH_ID.sed" not found.
@@ -14,15 +23,6 @@ if not exist "BRANCH_ID.sed" (
     exit
 ) else (
     echo file "BRANCH_ID.sed" found.
-    echo:
-)
-ping 192.0.2.0 -n 1 -w 3000 >NUL
-if not exist "TRANSACTION_CODE.sed" (
-    echo file "TRANSACTION_CODE.sed" not found.
-    echo Job terminated
-    exit
-) else (
-    echo file "TRANSACTION_CODE.sed" found.
     echo:
 )
 ping 192.0.2.0 -n 1 -w 3000 >NUL
@@ -44,20 +44,20 @@ if not exist "ACCOUNT_NUMBER.sed" (
     echo:
 )
 ping 192.0.2.0 -n 1 -w 3000 >NUL
-if not exist "TRANSACTION_ID.sed" (
-    echo file "TRANSACTION_ID.sed" not found.
+if not exist "TRXN_ID.sed" (
+    echo file "TRXN_ID.sed" not found.
     echo Job terminated
     exit
 ) else (
-    echo file "TRANSACTION_ID.sed" found.
+    echo file "TRXN_ID.sed" found.
     echo:
 )
 
+sortcl /SPECIFICATION=BANK_TRXN_TYPE.rcl
 sortcl /SPECIFICATION=BANK_BRANCH.rcl
-sortcl /SPECIFICATION=BANK_TRANSACTION_TYPE.rcl
 sortcl /SPECIFICATION=BANK_CUSTOMER.rcl
 sortcl /SPECIFICATION=BANK_ACCOUNT.rcl
-sortcl /SPECIFICATION=BANK_TRANSACTION.rcl
-sqlcmd -U IRIDEMO -P sorco78 -S 192.168.3.95,1444 -d support -i EnableFK_Multiple_Tables.sql
+sortcl /SPECIFICATION=BANK_TRXN.rcl
+sqlcmd -U BANK -P Sorco789 -S 192.168.3.95,1444 -d support -i EnableFK_Multiple_Tables.sql
 
 echo Job Ended
